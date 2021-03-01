baa927de53fe74a7a4bab3403d60adf659373f2d
Washington Correia <37412752+washingtonklebio@users.noreply.github.com>
2021-03-01 19:49:28 -0300

    Delete .nuxt directory

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as AtomExtraIncomeBar } from '../../components/atom/ExtraIncomeBar.vue'
-
-export const LazyAtomExtraIncomeBar = import('../../components/atom/ExtraIncomeBar.vue' /* webpackChunkName: "components/atom-extra-income-bar" */).then(c => c.default || c)
