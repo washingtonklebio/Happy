d5a5f8a6fcce9ccb12dbaa7f3b99811d294a6c03
Washington Correia <washington@fortics.com.br>
2021-03-03 21:52:00 -0300

    feat: header creation completed challenges

---
+++ components/atom/CompletedChallenges.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="flex justify-between items-center pb-5 border-b-2 border-gray-line mt-8 lg:mt-14 font-medium">
+		<p class="text-lg">
+			Horas concluídas
+		</p>
+		<p class="text-2xl">
+			12h 20m
+		</p>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+
+	export default Vue.extend({
+		name: 'CompletedChallenges',
+	});
+</script>
