1f88d15764150344de1cde778d34cf16fcb81902
Washington Correia <washington@fortics.com.br>
2021-03-03 21:57:43 -0300

    feat: creating cycle start component

--- components/molecules/Profile.vue
+++ components/molecules/Profile.vue
@@ -2,7 +2,7 @@
 	<div class="flex items-center">
 		<img src="https://github.com/washingtonklebio.png" class="mr-6 w-20 h-20 rounded-full">
 		<div class="flex flex-col w-full">
-			<strong class="text-title text-2xl">Washington Correia</strong>
+			<strong class="text-blue text-2xl">Washington Correia</strong>
 			<p class="flex items-center">
 				<img src="icons/level.svg" class="mr-2">
 				<span class="text-base"> R$ 199,00</span>
---
+++ components/molecules/StartCycle.vue
@@ -0,0 +1,11 @@
+<template>
+	<section class="flex flex-col flex-1 justify-center items-center p-10 md:p-16">
+		<h1 class="font-medium text-center text-2xl">
+			Avance em sua meta de leve
+		</h1>
+		<img src="icons/level-up.svg" alt="Level up" class="mt-10 md:mt-16" :style="{ minHeight: '70px' }">
+		<p class="text-center text-md mt-4">
+			Inicie um novo ciclo para alcançar seu objetivo mensal...
+		</p>
+	</section>
+</template>
